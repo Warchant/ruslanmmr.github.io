df790fd8c8ba09c8e9a9c6459b0399099dd86b48
ruslanmmr <belozerzevruslan@gmail.com>
2018-07-11 15:21:30 +0300

    edit

--- dist/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.btn').on('touchstart', function(e){
-    $(this).addClass('touch');
-});
-
-$('.btn').on('touchend', function(e){
-    $('this').removeClass('touch');
-});
-});
