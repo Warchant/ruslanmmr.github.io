1b543b8008d0359432117abbd7981941389077b3
ruslanmmr <belozerzevruslan@gmail.com>
2018-07-19 22:56:54 +0300

    desc

--- portfolio/Moda/dist/js/common.js
+++ portfolio/Moda/dist/js/common.js
@@ -4,3 +4,67 @@ $(document).ready(function() {
                     $(this).next(".bg").toggleClass("activebg");
                 });
             });
+
+$(document).ready(function() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        center: false,
+        rewind: false,
+        checkVisibility: true,
+        dots: false,
+
+        mouseDrag: true,
+        touchDrag: true,
+        pullDrag: true,
+        freeDrag: false,
+
+        margin: 0,
+        stagePadding: 0,
+
+        merge: false,
+        mergeFit: true,
+        autoWidth: false,
+
+        startPosition: 0,
+        rtl: false,
+
+        smartSpeed: 800,
+        fluidSpeed: false,
+        dragEndSpeed: false,
+
+        responsive: {},
+        responsiveRefreshRate: 200,
+        responsiveBaseElement: window,
+
+        fallbackEasing: 'swing',
+        slideTransition: '',
+
+        info: false,
+
+        nestedItemSelector: false,
+        itemElement: 'div',
+        stageElement: 'div',
+
+        refreshClass: 'owl-refresh',
+        loadedClass: 'owl-loaded',
+        loadingClass: 'owl-loading',
+        rtlClass: 'owl-rtl',
+        responsiveClass: 'owl-responsive',
+        dragClass: 'owl-drag',
+        itemClass: 'owl-item',
+        stageClass: 'owl-stage',
+        stageOuterClass: 'owl-stage-outer',
+        grabClass: 'owl-grab'
+    });
+    $('.arrow_r').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.arrow_l').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel');
+    })
+});
