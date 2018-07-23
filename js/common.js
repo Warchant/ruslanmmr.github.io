fe283431fb29472d4a895497a7d3244d6cec1521
ruslanmmr <belozerzevruslan@gmail.com>
2018-07-23 12:12:24 +0300

    ver1

--- js/common.js
+++ js/common.js
@@ -5,14 +5,33 @@ $(document).ready(function() {
         if ($(window).scrollTop() > 120) {
             $("#home .about").addClass("hidden");
             $("#home .bg").addClass("bg_active");
+            $("#home .arrow").addClass("hidden");
             $("#portfolio h2").css('color', 'rgb(20, 20, 20)');
             
         } else {
             $("#home .bg").removeClass("bg_active");
             $("#home .about").removeClass("hidden");
+            $("#home .arrow").removeClass("hidden");
             $("#portfolio h2").css('color', 'transparent');
             $("#wrapper").css('transition', '.2s');
         }
+    });
+});
+
+$(document).ready(function(){
+    $('.arrow_a, .logo a').click( function(){ 
+    var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) { 
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); 
+        }
+        return false; 
+    });
+});
+
+$(document).ready(function() {
+    $(".block .content").hover(function() {
+        $(this).toggleClass("onhover");
+
     });
 });
 
