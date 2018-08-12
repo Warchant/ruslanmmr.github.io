5af2fb83be79eb1bdc81896829ffd6fcdcd668d6
ruslanmmr <belozerzevruslan@gmail.com>
2018-08-12 19:00:46 +0300

    added

---
+++ portfolio/Veliki/app/js/common.js
@@ -0,0 +1,66 @@
+$('.popup_img').magnificPopup({
+  type: 'image'
+  // other options
+});
+
+$('.btn').magnificPopup({
+  items: {
+      src: '#popup',
+      type: 'inline'
+  }
+});
+
+$(document).ready(function() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 2,
+        loop: true,
+        center: false,
+        rewind: false,
+        checkVisibility: true,
+        dots: true,
+
+        mouseDrag: true,
+        touchDrag: true,
+        pullDrag: true,
+        freeDrag: false,
+
+        margin: 0,
+        stagePadding: 0,
+
+        merge: false,
+        mergeFit: true,
+        autoWidth: false,
+
+        startPosition: 0,
+        rtl: false,
+
+        smartSpeed: 800,
+        fluidSpeed: false,
+        dragEndSpeed: false,
+
+        responsive: {},
+        responsiveRefreshRate: 200,
+        responsiveBaseElement: window,
+
+        fallbackEasing: 'swing',
+        slideTransition: '',
+
+        info: false,
+
+        nestedItemSelector: false,
+        itemElement: 'div',
+        stageElement: 'div',
+
+        refreshClass: 'owl-refresh',
+        loadedClass: 'owl-loaded',
+        loadingClass: 'owl-loading',
+        rtlClass: 'owl-rtl',
+        responsiveClass: 'owl-responsive',
+        dragClass: 'owl-drag',
+        itemClass: 'owl-item',
+        stageClass: 'owl-stage',
+        stageOuterClass: 'owl-stage-outer',
+        grabClass: 'owl-grab'
+    });
+});
