f0ac0fd6e459ac7474649200f45afd4b6629881c
ruslanmmr <belozerzevruslan@gmail.com>
2018-08-17 12:38:48 +0300

    added contacts

--- portfolio/Barbershop/app/js/common.js
+++ portfolio/Barbershop/app/js/common.js
@@ -11,11 +11,18 @@ $(document).ready(function() {
         $("#navigation").toggleClass("nav_active");
         $("body").removeClass("wrapper_active");
     });
+    $('#navigation .contacts').click(function(event) {
+        event.preventDefault();
+        $(".nav-btn a").toggleClass("a_active");
+        $("#navigation").toggleClass("nav_active");
+        $("body").removeClass("wrapper_active");
+    });
 });
 
 $(document).ready(function() {
 	  $(".moscow_page, .spb_page, .reg_page").hide();
 	  $(".bg_page_other").hide();
+      $("#contacts_tab").hide();
     $('#home .tab_names .msc, #navigation .cena').click(function() {
         $("#home .tab_names p").removeClass("active");
     	  $("#home .tab_names .msc").addClass("active");
@@ -41,11 +48,29 @@ $(document).ready(function() {
     	  $(".bg_page_other").fadeIn(1000);
     });
       $('#header .logo a').click(function() {
+        $("#contacts_tab").hide();
+         $("#home, .bg_page_home").fadeIn(1000);
         $("#home .tab_names p").removeClass("active");
     	  $(".reg_page, .spb_page, .moscow_page").hide();
     	  $(".home_page").fadeIn(1000);
     	  $(".bg_page_home").removeClass("other");
     	  $(".bg_page_other").hide();
     	  $(".bg_page_home").fadeIn(1000);
+    });
+      $('#navigation .contacts').click(function() {
+          $("#home, .bg_page_home").hide();
+          $("#contacts_tab").fadeIn(1000);
+    });
+    $('#navigation .cena').click(function() {
+           $("#contacts_tab").hide();
+         $("#home").fadeIn(1000);
+    });
+    $('#navigation .current').click(function(event) {
+         event.preventDefault();
+           $("#contacts_tab").hide();
+         $("#home").fadeIn(1000);
+         $(".nav-btn a").toggleClass("a_active");
+        $("#navigation").toggleClass("nav_active");
+        $("body").removeClass("wrapper_active");
     });
 });
