23cc96f6ec7a1df08ef5d3f166030456dafa3e01
ruslanmmr <belozerzevruslan@gmail.com>
2018-08-21 12:01:26 +0300

    new ver

--- portfolio/Barbershop/app/js/common.js
+++ portfolio/Barbershop/app/js/common.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('.nav-btn a').click(function(event) {
         event.preventDefault();
@@ -5,24 +6,11 @@ $(document).ready(function() {
         $("#navigation").toggleClass("nav_active");
         $("body").toggleClass("wrapper_active");
     });
-    $('#navigation .cena').click(function(event) {
-        event.preventDefault();
-        $(".nav-btn a").toggleClass("a_active");
-        $("#navigation").toggleClass("nav_active");
-        $("body").removeClass("wrapper_active");
-    });
-    $('#navigation .contacts').click(function(event) {
-        event.preventDefault();
-        $(".nav-btn a").toggleClass("a_active");
-        $("#navigation").toggleClass("nav_active");
-        $("body").removeClass("wrapper_active");
-    });
 });
 
 $(document).ready(function() {
 	  $(".moscow_page, .spb_page, .reg_page").hide();
 	  $(".bg_page_other").hide();
-      $("#contacts_tab").hide();
     $('#home .tab_names .msc, #navigation .cena').click(function() {
         $("#home .tab_names p").removeClass("active");
     	  $("#home .tab_names .msc").addClass("active");
@@ -48,8 +36,8 @@ $(document).ready(function() {
     	  $(".bg_page_other").fadeIn(1000);
     });
       $('#header .logo a').click(function() {
-        $("#contacts_tab").hide();
          $("#home, .bg_page_home").fadeIn(1000);
+         $(".tab_page").not(":first").hide();
         $("#home .tab_names p").removeClass("active");
     	  $(".reg_page, .spb_page, .moscow_page").hide();
     	  $(".home_page").fadeIn(1000);
@@ -57,29 +45,20 @@ $(document).ready(function() {
     	  $(".bg_page_other").hide();
     	  $(".bg_page_home").fadeIn(1000);
     });
-      $('#navigation .contacts').click(function() {
-          $("#home, .bg_page_home").hide();
-          $("#contacts_tab").fadeIn(1000);
-    });
-    $('#navigation .cena').click(function() {
-           $("#contacts_tab").hide();
-         $("#home").fadeIn(1000);
-    });
-    $('#navigation .current').click(function(event) {
-         event.preventDefault();
-           $("#contacts_tab").hide();
-         $("#home").fadeIn(1000);
-         $(".nav-btn a").toggleClass("a_active");
-        $("#navigation").toggleClass("nav_active");
-        $("body").removeClass("wrapper_active");
-    });
 });
 
 
 //табы проектов
 
-$("#home .img .img_box").not(":first").hide();
-        $("#home .projects_content .block").click(function() {
-            $("#home .projects_content .block").removeClass("active").eq($(this).index()).addClass("active");
-            $("#home .img .img_box").hide().eq($(this).index()).fadeIn(300)
+$("#projects .img .img_box").not(":first").hide();
+        $("#projects .projects_content .block").click(function() {
+            $("#projects .projects_content .block").removeClass("active").eq($(this).index()).addClass("active");
+            $("#projects .img .img_box").hide().eq($(this).index()).fadeIn(300)
+}).eq(0).addClass("active");
+
+$(".tab_page").not(":first").hide();
+        $("#navigation .link").click(function(event) {
+            event.preventDefault();
+            $(this).removeClass("link");
+            $(".tab_page").hide().eq($(this).index()).fadeIn(1000)
 }).eq(0).addClass("active");
