9dfc5e0af30081402ef1d58d53bfcb3dffa29bb1
ruslanmmr <belozerzevruslan@gmail.com>
2018-08-22 19:14:55 +0300

    added

--- portfolio/CSA/app/js/common.js
+++ portfolio/CSA/app/js/common.js
@@ -1,3 +1,20 @@
-$(function() {
+$(function(){
+  $('#home_page .logo_content').width($('.logo_content').height());
 
+  $(window).resize(function(){
+    $('#home_page .logo_content').width($('.logo_content').height());
+  });
+});
+
+$('.nav-btn a').click(function(event) {
+        event.preventDefault();
+        $(".nav-btn a").toggleClass("a_active");
+        $("#mobile_nav").toggleClass("active_nav");
+        $("body").toggleClass("body_active");
+    });
+
+$('#header_nav .link_trigger a').click(function(event) {
+	  event.preventDefault();
+	  $("#header_nav .link_trigger a").parent().removeClass("active_link");
+    $(this).parent().addClass("active_link");
 });
