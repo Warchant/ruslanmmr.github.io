a78331e864957b84227f4bfef1ec1769254fd8a2
ruslanmmr <belozerzevruslan@gmail.com>
2018-08-30 11:41:24 +0300

    ver2

--- js/common.js
+++ js/common.js
@@ -1,76 +1,26 @@
+ $("header .content .btn").click(function(event) {
+             event.preventDefault();
+            $("header .content .btn").removeClass("active").eq($(this).index()).addClass("active");
+            $(".tab_name").hide().removeClass("animated").eq($(this).index()).addClass("animated").fadeIn(1000);
+        })
 
 $(document).ready(function() {
     $(".portf").click(function(event) {
-        event.preventDefault();
-        $("#wrapper").addClass("wrapper_a");
-        $("#portfolio").addClass("p_active");
-        $("#home").removeClass("home_active_left");
-        $("#home").addClass("home_active_right");
-        $("#contacts").removeClass("c_active");
         $("#footer").addClass("f_active");
-        $(".bg").addClass("bg_active");
-        $("#contacts .content").addClass("c_hide");  
-        $("#portfolio .content").removeClass("c_hide");      
-        setTimeout(function() {
-            $(".hm").removeClass("active");
-            $(".cntcts").removeClass("active");
-            $(".portf").addClass("active");
-            $("body").addClass("body_active");
-        }, 700);
         setTimeout(function() {
             $('header').addClass("active_header_bg");
         }, 300);
     });
     $(".cntcts").click(function(event) {
-        event.preventDefault();
-        $("#portfolio").removeClass("p_active");
-        $("#home").removeClass("home_active_right");
-        $("#home").addClass("home_active_left");
-        $("#contacts").addClass("c_active");
-        $(".bg").removeClass("bg_active")
-        $("body").removeClass("body_active");
-        $('header').removeClass("active_header_bg");
-        $("#contacts .content").removeClass("c_hide"); 
-        $("#portfolio .content").addClass("c_hide");
-        setTimeout(function() {
-            $(".hm").removeClass("active");
-            $(".cntcts").addClass("active");
-            $(".portf").removeClass("active");
-            $("#wrapper").removeClass("wrapper_a");
-            $('body,html').animate({
-                scrollTop: 0
-            }, 400);
-            return false;
-        }, 700);
         setTimeout(function() {
             $("#footer").removeClass("f_active");
         }, 200);
     });
     $(".hm").click(function(event) {
-        event.preventDefault();
-        $("#portfolio").removeClass("p_active");
-        $("#home").removeClass("home_active_right");
-        $("#home").removeClass("home_active_left");
-        $("#contacts").removeClass("c_active");
-        $("body").addClass("body_active");
-        $("body").removeClass("body_active");
-        $(".bg").removeClass("bg_active");
         $('header').removeClass("active_header_bg");
-        $("#contacts .content").addClass("c_hide"); 
-        $("#portfolio .content").addClass("c_hide"); 
         setTimeout(function() {
             $("#footer").removeClass("f_active");
         }, 200);
-        setTimeout(function() {
-            $(".hm").addClass("active");
-            $(".cntcts").removeClass("active");
-            $(".portf").removeClass("active");
-            $("#wrapper").removeClass("wrapper_a");
-            $('body,html').animate({
-                scrollTop: 0
-            }, 400);
-            return false;
-        }, 700);
     });
 });
 
