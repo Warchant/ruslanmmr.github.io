be766350d48d21378347dd461d722256f348ba6f
ruslanmmr <belozerzevruslan@gmail.com>
2018-09-27 23:09:29 +0300

    ed

--- portfolio/rofl/app/js/common.js
+++ portfolio/rofl/app/js/common.js
@@ -2,7 +2,25 @@ $(function() {
     $("a").click(function() {
         $(this).addClass("hinge");
         setTimeout(function(){
-             $("body").addClass("game_over");
+             $(".post").addClass("game_over");
         }, 1000);
+         setTimeout(function(){
+             $(".lin1").addClass("active");
+        }, 2000);
+          setTimeout(function(){
+             $(".lin2").addClass("active");
+        }, 3000);
+           setTimeout(function(){
+             $(".lin3").addClass("active");
+        }, 4000);
+            setTimeout(function(){
+             $(".lin4").addClass("active");
+        }, 5000);
+        setTimeout(function(){
+             $(".post").addClass("end");
+        }, 6000);
+        setTimeout(function(){
+             $(".post").addClass("hinge");
+        }, 7000);
     });
 });
