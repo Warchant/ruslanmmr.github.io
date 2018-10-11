fabcfaa1e051b27953726c1b368adcbb53c5dd76
ruslanmmr <belozerzevruslan@gmail.com>
2018-10-11 03:46:24 +0300

    added csa

--- portfolio/CSA/app/js/common.js
+++ portfolio/CSA/app/js/common.js
@@ -1,13 +1,33 @@
-//Общее
-
 $(document).ready(function() {
+    navigationClick();
+    homepageSlider();
+    footerFunc();
+    dopSliders();
+    withCards();
+    if (document.documentElement.clientWidth > 1025) {
+        navigationScroll();
+        lastSlider();
+        gallerySlider();
+        autoWith();
+    }
+    if (document.documentElement.clientWidth < 1026) {
+        bgFixed();
+        mobileScroll();
+    }
+});
+
+var jqBar = $('#last_page').position().top;
+var totalWheel = 0,
+    once = 1;
+
+//Вся навигация по кликам
+function navigationClick() {
     $('.nav-btn a').click(function(event) {
         event.preventDefault();
         $(".nav-btn a").toggleClass("a_active");
         $("#mobile_nav").toggleClass("active_nav");
         $("body").toggleClass("body_active");
     });
-
     $('#header_nav .link_trigger a').click(function(event) {
         event.preventDefault();
         $("#header_nav .link_trigger a").parent().removeClass("active_link");
@@ -21,8 +41,6 @@ $(document).ready(function() {
         $("#header_nav .left_section .icons_block a, #header_nav .first_link .link a").removeClass("active_icon");
         $(this).addClass("active_icon");
     });
-
-
     $('#header_nav .first_link .link a, #header_nav .fr_link a').click(function(event) {
         event.preventDefault();
         $(".header_nav").addClass("header_nav_transformed");
@@ -33,7 +51,6 @@ $(document).ready(function() {
         $("#header_top").removeClass("header_top_last");
         totalWheel = 700;
     });
-
     $('#header_nav .container .header_nav .right_section ul li a').click(function(event) {
         $(".header_nav").addClass("header_nav_transformed");
         $(".header_top").addClass("header_top_transformed");
@@ -45,7 +62,6 @@ $(document).ready(function() {
         once = 2;
         $("#start_page").css("width", "0%");
     });
-
     $('#header_top .header_top .logo a, #footer .logo a').click(function(event) {
         event.preventDefault();
         $(".header_nav").removeClass("header_nav_transformed");
@@ -60,17 +76,17 @@ $(document).ready(function() {
         $("#start_page").css("width", "100%");
         totalWheel = 0;
     });
+};
 
-    var totalWheel = 0;
-    var once = 1;
-
+//Вся навигация по скроллу (только колесо мыши)
+function navigationScroll() {
     window.onwheel = function(e) {
         var delta = e.deltaY;
         totalWheel += delta;
         if (totalWheel < 0) {
             totalWheel = 0;
             $("#start_page").removeClass("active");
-                $("#start_page").css("width", "100%");
+            $("#start_page").css("width", "100%");
         } else if ((document.documentElement.clientWidth > 1025) && totalWheel > 0 && totalWheel < 700) {
             once = 1;
             $("#home_page").fadeIn(500);
@@ -123,9 +139,10 @@ $(document).ready(function() {
             }
         }
     }
+};
 
-    //страница вторая
-
+//страница вторая(слайдеры и прочее)
+function homepageSlider() {
     $('.slider1').slick({
         dots: false,
         infinite: false,
@@ -153,6 +170,7 @@ $(document).ready(function() {
     });
 
     $('.slider1').on('afterChange', function(event, slick, currentSlide) {
+        jqBar = $('#last_page').position().top;
         if (currentSlide == 1) {
             $("#home_page .arrow_right1 span, #home_page .arrow_left1 span").fadeIn(300);
         } else {
@@ -189,9 +207,10 @@ $(document).ready(function() {
         e.preventDefault();
         $('.slider1').slick('slickGoTo', 1);
     });
+};
 
-    //третья страница
-
+//третья страница
+function lastSlider() {
     $('.slider2').slick({
         dots: false,
         infinite: false,
@@ -201,7 +220,6 @@ $(document).ready(function() {
         arrows: false,
         draggable: false
     });
-
     $('#header_nav .right_section .link1, #last_page .custom_dots ul .dot1').click(function(e) {
         e.preventDefault();
         $('.slider2').slick('slickGoTo', 0);
@@ -229,8 +247,10 @@ $(document).ready(function() {
         $("#header_nav .link_trigger a").parent().removeClass("active_link");
         $("#header_nav .right_section .link3").addClass("active_link");
     });
+}
 
-    //слайдер новостей первого блока
+//слайдер новостей первого блока
+function dopSliders() {
     $('.slider_news').slick({
         dots: false,
         infinite: true,
@@ -240,40 +260,6 @@ $(document).ready(function() {
         prevArrow: $('.arrow_left_slide1'),
         nextArrow: $('.arrow_right_slide1')
     });
-    $('.slider3').slick({
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        arrows: false
-        //prevArrow: $('.arrow_left_slide1'),
-        //nextArrow: $('.arrow_right_slide1')
-    });
-    $('.slider_statistic').slick({
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 9,
-        slidesToScroll: 5,
-        arrows: true,
-        vertical: true,
-        verticalSwiping: true,
-        nextArrow: $('#last_page .container .slider2 .slide4 .content .center_block .arrow_bottom'),
-        prevArrow: $('')
-    });
-    $('.slider_gallery').slick({
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        vertical: true,
-        verticalSwiping: true,
-        nextArrow: $('.btn_bottom'),
-        prevArrow: $('')
-    });
     $('.photo_slider').slick({
         dots: false,
         speed: 500,
@@ -281,10 +267,65 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-        nextArrow: $('.arrow_left_photo'),
-        prevArrow: $('.arrow_right_photo')
+        nextArrow: $('.arrow_right_photo'),
+        prevArrow: $('.arrow_left_photo'),
+        responsive: [{
+                breakpoint: 1025,
+                settings: {
+                    swipe: true
+                }
+            },
+            {
+                breakpoint: 0,
+                settings: {}
+            }
+        ]
     });
-});
+}
+
+function gallerySlider() {
+    $('.slider_gallery').slick({
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: true,
+        vertical: true,
+        verticalSwiping: true,
+        nextArrow: $('.btn_bottom'),
+        prevArrow: $('')
+    });
+}
+
+function mobileScroll() {
+    var $page = $('html, body');
+    $('#mobile_nav ul a').click(function() {
+        $(".nav-btn a").removeClass("a_active");
+        $("#mobile_nav").removeClass("active_nav");
+        $("body").removeClass("body_active");
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+}
+
+function bgFixed() {
+    $(function() {
+        var bg = $('#last_page .active_bg');
+        var jqBarStatus = true;
+        $(window).scroll(function() {
+            if (($(window).scrollTop() > jqBar)) {
+                bg.addClass("active_bg_fixed");
+                jqBarStatus = false;
+            } else if (($(window).scrollTop() < jqBar) && jqBarStatus == false) {
+                bg.removeClass("active_bg_fixed");
+                jqBarStatus = true;
+            }
+        });
+    });
+}
 
 
 //блоки колезеев
@@ -341,24 +382,24 @@ function autoWith() {
     $('.slide3 .place_block .img_block').width($(".slide3 .place_block .img_block").height());
 }
 
-$(document).ready(function() {
-    autoWith();
+function withCards() {
     $.when(sliderPlace()).then(function() {
         setTimeout(function() {
             placeTabs();
         }, 1000);
     });
-});
+};
 
 
-
-$(document).mousemove(function(e){
-    var Y = e.pageY;
-    var X = $('#wrapper').height();
-    var Z = X - Y;
-    if ( Z < 15  ) {
-        $("#header_nav, #footer").addClass("active_footer"); 
-    } else if (Z > 154) {
-        $("#header_nav, #footer").removeClass("active_footer"); 
-    }
-}); 
+function footerFunc() {
+    $(document).mousemove(function(e) {
+        var Y = e.pageY;
+        var X = $('#wrapper').height();
+        var Z = X - Y;
+        if (Z < 15) {
+            $("#header_nav, #footer").addClass("active_footer");
+        } else if (Z > 154) {
+            $("#header_nav, #footer").removeClass("active_footer");
+        }
+    });
+};
