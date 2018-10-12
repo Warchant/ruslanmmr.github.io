35954986b017f63b01b3c913971018fe54460551
ruslanmmr <belozerzevruslan@gmail.com>
2018-10-12 03:36:16 +0300

    added

--- portfolio/Crystal/app/js/common.js
+++ portfolio/Crystal/app/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
      bannerSlider();
+     labsSlider();
 });
 
 function bannerSlider() {
@@ -13,5 +14,18 @@ function bannerSlider() {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000
+    });
+};
+
+function labsSlider() {
+    $('.slider_labs').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: $('.arrow_left'),
+        nextArrow: $('.arrow_right'),
     });
 };
