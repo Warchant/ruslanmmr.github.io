423eca44b63ed06d527b5c2e9c5853f88166efe3
ruslanmmr <belozerzevruslan@gmail.com>
2018-11-06 01:02:58 +0200

    eddited

--- portfolio/Individual/app/js/common.js
+++ portfolio/Individual/app/js/common.js
@@ -22,17 +22,15 @@ function Nav() {
 	console.log(height);
   if (height < 550) {
     $(".nav_mobile").css('overflow-y', 'scroll');
-    $("header").css('position', 'fixed');
   } else {
     $(".nav_mobile").css('overflow-y', 'visible');
-    $("header").css('position', 'absolute');
   }
 }
 
 function openNav() {
   $('.nav-btn').click(function(event) {
     event.preventDefault();
-    $(".nav-btn a").toggleClass("a_active");
+    $(".nav-btn").toggleClass("nav_btn_active");
     $(".nav_mobile").toggleClass("nav_active");
     $("body").toggleClass("body_fixed");
   });
