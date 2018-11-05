5819aeade44418c553290ec455e03e20d69c0c9e
ruslanmmr <belozerzevruslan@gmail.com>
2018-11-05 23:54:39 +0200

    added individual

---
+++ portfolio/Individual/app/js/common.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  resizeNav();
+  Nav();
+  openNav();
+});
+
+$(window).resize(function() {
+  resizeNav();
+  //Nav();
+});
+
+function resizeNav() {
+  if ($(window).width() < 768) {
+    $('.nav_content').appendTo('.nav_mobile');
+  } else {
+    $('.nav_content').insertAfter('.center_logo');
+  }
+}
+
+function Nav() {
+  if ($(document).height() < 550) {
+    $(".nav_mobile").css('overflow-y', 'scroll');
+  } else {
+    $(".nav_mobile").css('overflow-y', 'visible');
+  }
+}
+
+function openNav() {
+  $('.nav-btn a').click(function(event) {
+    event.preventDefault();
+    $(".nav-btn a").toggleClass("a_active");
+    $(".nav_mobile").toggleClass("nav_active");
+    $("body").toggleClass("wrapper_active");
+  });
+}
