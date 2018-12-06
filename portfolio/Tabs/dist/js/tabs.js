a145c8646893dc007044c86fef932fde8584880a
ruslanmmr <belozerzevruslan@gmail.com>
2018-12-06 13:08:19 +0200

    some projects deleted

--- portfolio/Tabs/dist/js/tabs.js
+++
@@ -1,6 +0,0 @@
-jQuery:
-            $(".tab_item").not(":first").hide();
-        $(".wrapper .tab").click(function() {
-            $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-            $(".tab_item").hide().eq($(this).index()).fadeIn(300)
-        }).eq(0).addClass("active");
